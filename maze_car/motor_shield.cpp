855f87122cf896bb69dad846ec8a3574b3613e22
Bloem <sem@ie-design.nl>
2025-02-21 23:04:43 +0100

    made some useless stuff a comment

--- maze_car/motor_shield.cpp
+++ maze_car/motor_shield.cpp
@@ -32,13 +32,16 @@ void Motor_Shield::update_speed()
   // pinMode(7, OUTPUT);
   uint8_t motors_d[] = {0x04,0x10,0x01,0x40 };
   uint8_t motors_r[] = {0x08,0x02,0x40,0x80 };
-
+// 0 == RF
+// 1 == LF
+// 2 == RB
+// 3 == LB
   for(uint8_t m = 0; m < 4; m++)
   {
     // Only the first one does something, and it resets when another is written to
     // motor_state = motor_state_(m, 1);
    
-    pinMode(3, OUTPUT);
+    // pinMode(3, OUTPUT);
     // digitalWrite(7, LOW);
     // digitalWrite(12, LOW);
     // shiftOut(8, CLOCK_PIN, MSBFIRST, 255);
@@ -46,7 +49,7 @@ void Motor_Shield::update_speed()
     // shiftOut(8, CLOCK_PIN, MSBFIRST, MotorsR[m]);
     // digitalWrite(12, HIGH, motors_d[m]);
     // digitalWrite(7, HIGH);
-    // delay(250);
+    delay(250);
   }
   // digitalWrite(ENABLE_PIN, LOW);
 }
@@ -56,34 +59,34 @@ void Motor_Shield::update_single_speed()
   
 }
 
-uint8_t Motor_Shield::motor_state_(uint8_t motor, uint8_t direction)
-{
-  uint8_t state = 0;
-  delay(250);
-  switch(motor)
-  {
-    case M_LF:
-      state &= ~(1 << LF1) & ~(1 << LF2);
-      state |= 1 << LF1;
-      state &= ~(1 << LF2);
-      return state;
-    case M_RF:
-      state &= ~(1 << RF1) & ~(1 << RF2);
-      state |= 1 << RF1;
-      state &= ~(1 << RF2);
-      return state;
-    case M_LB:
-      state &= ~(1 << LB1) & ~(1 << LB2);
-      state |= 1 << LB1;
-      state &= ~(1 << LB2);
-      return state;
-    case M_RB:
-      state &= ~(1 << RB1) & ~(1 << RB2);
-      state |= 1 << RB1;
-      state &= ~(1 << RB2);
-      return state;
-  }
-}
+// uint8_t Motor_Shield::motor_state_(uint8_t motor, uint8_t direction)
+// {
+//   uint8_t state = 0;
+//   delay(250);
+//   switch(motor)
+//   {
+//     case M_LF:
+//       state &= ~(1 << LF1) & ~(1 << LF2);
+//       state |= 1 << LF1;
+//       state &= ~(1 << LF2);
+//       return state;
+//     case M_RF:
+//       state &= ~(1 << RF1) & ~(1 << RF2);
+//       state |= 1 << RF1;
+//       state &= ~(1 << RF2);
+//       return state;
+//     case M_LB:
+//       state &= ~(1 << LB1) & ~(1 << LB2);
+//       state |= 1 << LB1;
+//       state &= ~(1 << LB2);
+//       return state;
+//     case M_RB:
+//       state &= ~(1 << RB1) & ~(1 << RB2);
+//       state |= 1 << RB1;
+//       state &= ~(1 << RB2);
+//       return state;
+//   }
+// }
 
 void Motor_Shield::shift_out(uint8_t data)
 {
@@ -92,11 +95,11 @@ void Motor_Shield::shift_out(uint8_t data)
   for(uint8_t i = 0; i < 8; i++)
   {
     digitalWrite(CLOCK_PIN, LOW);
-    if(data & (1 << (7 - i)))
-      digitalWrite(DATA_PIN, HIGH);
-    else
-      digitalWrite(DATA_PIN, LOW);
-    // digitalWrite(DATA_PIN, (data & (1 << (7 - i)) ? HIGH : LOW));
+    // if(data & (1 << (7 - i)))
+    //   digitalWrite(DATA_PIN, HIGH);
+    // else
+    //   digitalWrite(DATA_PIN, LOW);
+    digitalWrite(DATA_PIN, (data & (1 << (7 - i)) ? HIGH : LOW));
     digitalWrite(CLOCK_PIN, HIGH);
   }
   digitalWrite(MOTORLATCH, HIGH);
