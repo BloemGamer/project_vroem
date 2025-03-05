381c1041288c46b5c9e557a2be90f7b0d18b48db
Bloem <sem@ie-design.nl>
2025-03-05 17:09:55 +0100

    added comments to the functions

--- maze_car/motor_shield.cpp
+++ maze_car/motor_shield.cpp
@@ -2,7 +2,8 @@
 #include "motor_shield.h"
 
 // bitwise black magic
-//
+// every motor has a forwards and backwards bit in the motor_state uint8_t, and this function fixes that
+// only one of those two bits is on, and the right one, and is break, both are off.
 #define change_motor_dir(a, b, dir) \
 switch(dir) \
 { \
@@ -18,7 +19,7 @@ switch(dir) \
 } 
 
 
-
+// Set the PWM pin of the motors on the right value
 void Motor_Shield::set_speed(uint8_t speed_lf, uint8_t speed_rf, uint8_t speed_lb, uint8_t speed_rb)
 {
 #ifdef ARDUINO
@@ -46,6 +47,7 @@ void Motor_Shield::set_speed(int8_t motor, uint8_t speed_m)
 #endif // ARDUINO 
 }
 
+// changing the speed
 void Motor_Shield::change_speed(int8_t speed_lf, int8_t speed_rf, int8_t speed_lb, int8_t speed_rb)
 {
 #ifdef ARDUINO
@@ -75,27 +77,22 @@ void Motor_Shield::change_speed(int8_t motor, int8_t speed_m)
 
 void Motor_Shield::update_speed()
 {
-  shift_out(motor_state);
-}
 
-inline void Motor_Shield::shift_out(uint8_t data)
-{
   digitalWrite(MOTORLATCH, LOW);
   digitalWrite(DATA_PIN, LOW);
   for(uint8_t i = 0; i < 8; i++)
   {
     digitalWrite(CLOCK_PIN, LOW);
-    digitalWrite(DATA_PIN, (data & (1 << (7 - i)) ? HIGH : LOW));
+    digitalWrite(DATA_PIN, (motor_state & (1 << (7 - i)) ? HIGH : LOW));
     digitalWrite(CLOCK_PIN, HIGH);
   }
   digitalWrite(MOTORLATCH, HIGH);
 }
 
-
-
+// setting the pins on output and setting the speed on full
 Motor_Shield::Motor_Shield(void)
 {
-// zet de pinnen op input/output van wat ze moeten
+// set pins on right mode
   pinMode(MOTORLATCH, OUTPUT);
   pinMode(ENABLE_PIN, OUTPUT);
   pinMode(DATA_PIN, OUTPUT);
@@ -103,6 +100,7 @@ Motor_Shield::Motor_Shield(void)
 
   digitalWrite(ENABLE_PIN, LOW);
 #ifdef ARDUINO // this one is just so my PC doesn't give a lot of errors
+  // setting speed on full
   OCR1A = 255;
   OCR3C = 255;
   OCR4A = 255;
@@ -115,6 +113,7 @@ Motor_Shield::Motor_Shield(void)
 #endif // Arduino
 }
 
+// changing the direction of the motor
 void Motor_Shield::change_motor_direction(uint8_t dir1, uint8_t dir2, uint8_t dir3, uint8_t dir4)
 {
   change_motor_dir(LF1, LF2, dir1);
@@ -124,6 +123,7 @@ void Motor_Shield::change_motor_direction(uint8_t dir1, uint8_t dir2, uint8_t di
   update_speed();
 }
 
+// destructer of the class, doesn't do anything at the moment
 Motor_Shield::~Motor_Shield()
 {
   
