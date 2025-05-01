5c5124cf7ab4b6aae982b7b35b3d071a6ea3b323
berend4952 <berendvankoedam@gmail.com>
2025-05-01 13:26:06 +0200

    added things

---
+++ maze_car/accelerometer.cpp
@@ -0,0 +1,10 @@
+
+int16_t Accelerometer::get_yaw(void)
+{
+  
+}
+
+int8_t Accelerometer::get_forwards_movement(void)
+{
+  
+}
---
+++ maze_car/accelerometer.h
@@ -0,0 +1,8 @@
+class Accelerometer
+{
+private:
+
+public:
+  int16_t get_yaw(void);
+  int8_t get_forwards_movement(void);
+};
