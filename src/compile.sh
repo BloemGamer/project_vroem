687abf24742250545f3a1546cb7ae1fdcecfac80
Bloem <sem@ie-design.nl>
2025-06-29 19:24:38 +0200

    added sudo, because otherwise it does not work

--- src/compile.sh
+++ src/compile.sh
@@ -6,7 +6,7 @@ g++ -o src/see_map src/see_map.cpp -Wall -g
 # Check if compilation succeeded
 if [ $? -eq 0 ]; then
     echo -e "Compilation finished successfully.\n"
-    ./src/see_map
+    sudo ./src/see_map
 else
     echo -e "Compilation failed.\n"
 fi
